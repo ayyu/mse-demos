ccc7408f1672ef8c5c5ea7dd84afc28321066fe9
ayyu <ayyu@uwaterloo.ca>
2022-02-12 14:34:30 -0500

    initial commit

---
+++ buffering/server.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import http from "http";
+import fs, { ReadStream } from "fs";
+import path from "path";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './demos/buffering/views');
+
+app.use(express.static('views'));
+
+const server = http.createServer(app);
+
+app.get('/', (_req, res) => {
+	res.render('./client.ejs');
+});
+
+app.get('/audio', (req, res) => {
+	let file = path.resolve(__dirname, './media/audio.webm');
+	let stream: ReadStream = fs.createReadStream(file);
+	res.writeHead(206, {
+		'Connection': 'close',
+		'Accept-Ranges': 'bytes',
+		'Transfer-Encoding':'chunked',
+	});
+	stream.pipe(res);
+});
+
+app.get('/video', (req, res) => {
+	let file = path.resolve(__dirname, './media/video.webm');
+	let stream: ReadStream = fs.createReadStream(file, {
+		end: 1000000,
+	});
+	res.writeHead(206, {
+		'Connection': 'close',
+		'Accept-Ranges': 'bytes',
+		'Transfer-Encoding':'chunked',
+	});
+	stream.pipe(res);
+});
+
+server.listen(3000);
